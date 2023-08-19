59ca7bd50289c7f3161e4fa8e8d7a6ab546293e2
hababa2 <zactanpeterson@gmail.com>
2023-08-19 12:14:02 -0600

    Fixed Chunk Management

--- Assets/Scripts/UI/Title.cs
+++ Assets/Scripts/UI/Title.cs
@@ -8,6 +8,6 @@ public class Title : MonoBehaviour
 		float sin1 = Mathf.Sin(Time.time * 4.0f);
 
         transform.rotation = Quaternion.Euler(0, 0, sin0 * 3.0f);
-		transform.localScale = Vector3.one * (sin1 + 3.0f) / 3.0f;
+		transform.localScale = Vector3.one * (sin1 / 2.0f + 1.5f);
     }
 }
