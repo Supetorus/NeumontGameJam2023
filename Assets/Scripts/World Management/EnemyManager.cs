3db8023c0b4340400cded7cf4c13b8dfbdf46d4a
hababa2 <zactanpeterson@gmail.com>
2023-08-19 08:57:12 -0600

    Better Spawning

--- Assets/Scripts/World Management/EnemyManager.cs
+++ Assets/Scripts/World Management/EnemyManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
 	private static readonly int MAX_ENEMIES = 20;
-	private static readonly float MAX_ENEMY_DISTANCE_X = 25.0f;
-	private static readonly float MAX_ENEMY_DISTANCE_Y = 20.0f;
+	private static readonly float MAX_ENEMY_DISTANCE_X = 15.0f;
+	private static readonly float MAX_ENEMY_DISTANCE_Y = 8.0f;
 
 	[SerializeField] private List<GameObject> enemyPrefabs = new List<GameObject>();
 
@@ -17,24 +17,54 @@ public class EnemyManager : MonoBehaviour
 		manager = FindFirstObjectByType<GameManager>();
 	}
 
-    void Update()
-    {
+	void Update()
+	{
+		int maxEnemies = (int)(MAX_ENEMIES + Time.realtimeSinceStartup / 12.0f);
+
 		Vector3 pos = manager.Player.transform.position;
 
-		foreach(GameObject enemy in enemies)
+		foreach (GameObject enemy in enemies)
 		{
-			if(enemy.transform.position.x > pos.x + MAX_ENEMY_DISTANCE_X) { enemy.transform.position += Vector3.left * MAX_ENEMY_DISTANCE_X * 2.0f; }
-			else if(enemy.transform.position.x < pos.x - MAX_ENEMY_DISTANCE_X) { enemy.transform.position += Vector3.right * MAX_ENEMY_DISTANCE_X * 2.0f; }
+			if (enemy.transform.position.x > pos.x + MAX_ENEMY_DISTANCE_X * 2.0f) { enemy.transform.position += Vector3.left * MAX_ENEMY_DISTANCE_X * 3.5f; }
+			else if (enemy.transform.position.x < pos.x - MAX_ENEMY_DISTANCE_X * 2.0f) { enemy.transform.position += Vector3.right * MAX_ENEMY_DISTANCE_X * 3.5f; }
 
-			if (enemy.transform.position.y > pos.y + MAX_ENEMY_DISTANCE_Y) { enemy.transform.position += Vector3.down * MAX_ENEMY_DISTANCE_Y * 2.0f; }
-			else if (enemy.transform.position.y < pos.y - MAX_ENEMY_DISTANCE_Y) { enemy.transform.position += Vector3.up * MAX_ENEMY_DISTANCE_Y * 2.0f; }
+			if (enemy.transform.position.y > pos.y + MAX_ENEMY_DISTANCE_Y * 2.0f) { enemy.transform.position += Vector3.down * MAX_ENEMY_DISTANCE_Y * 3.5f; }
+			else if (enemy.transform.position.y < pos.y - MAX_ENEMY_DISTANCE_Y * 2.0f) { enemy.transform.position += Vector3.up * MAX_ENEMY_DISTANCE_Y * 3.5f; }
 		}
 
-		if (enemies.Count < MAX_ENEMIES)
+		if (enemies.Count < maxEnemies)
 		{
-			enemies.Add(Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)], 
-				new Vector3(pos.x + Random.Range(-MAX_ENEMY_DISTANCE_X, MAX_ENEMY_DISTANCE_X), 
-				pos.y + Random.Range(-MAX_ENEMY_DISTANCE_Y, MAX_ENEMY_DISTANCE_Y), 0.0f), Quaternion.identity));
+			switch (Random.Range(0, 4))
+			{
+				case 0:
+					{
+						enemies.Add(Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)],
+							new Vector3(pos.x + Random.Range(-MAX_ENEMY_DISTANCE_X - 5.0f, -MAX_ENEMY_DISTANCE_X),
+							pos.y + Random.Range(-MAX_ENEMY_DISTANCE_Y, MAX_ENEMY_DISTANCE_Y), 0.0f), Quaternion.identity));
+					}
+					break;
+				case 1:
+					{
+						enemies.Add(Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)],
+							new Vector3(pos.x + Random.Range(MAX_ENEMY_DISTANCE_X, MAX_ENEMY_DISTANCE_X + 5.0f),
+							pos.y + Random.Range(-MAX_ENEMY_DISTANCE_Y, MAX_ENEMY_DISTANCE_Y), 0.0f), Quaternion.identity));
+					}
+					break;
+				case 2:
+					{
+						enemies.Add(Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)],
+							new Vector3(pos.x + Random.Range(-MAX_ENEMY_DISTANCE_X, MAX_ENEMY_DISTANCE_X),
+							pos.y + Random.Range(-MAX_ENEMY_DISTANCE_Y - 5.0f, -MAX_ENEMY_DISTANCE_Y), 0.0f), Quaternion.identity));
+					}
+					break;
+				case 3:
+					{
+						enemies.Add(Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)],
+							new Vector3(pos.x + Random.Range(-MAX_ENEMY_DISTANCE_X, MAX_ENEMY_DISTANCE_X),
+							pos.y + Random.Range(MAX_ENEMY_DISTANCE_Y, MAX_ENEMY_DISTANCE_Y + 5.0f), 0.0f), Quaternion.identity));
+					}
+					break;
+			}
 		}
-    }
+	}
 }
