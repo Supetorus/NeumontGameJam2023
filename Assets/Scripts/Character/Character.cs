f878cd61a9f05adb68dfdded6c8e34fe1c2c4fed
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2023-08-18 21:22:02 -0600

    character

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character : MonoBehaviour
+{
+	private SpriteRenderer m_Renderer;
+	private Rigidbody2D m_Rigidbody;
+	private CircleCollider2D m_Collider;
+
+	private GameObject m_WeaponObject;
+	private SpriteRenderer m_WeaponSprite;
+
+	[Header("Renderer")]
+	[SerializeField]
+	private Sprite m_Sprite;
+
+	[Header("Movement")]
+	[SerializeField]
+	private float m_WalkSpeed = 2;
+	[SerializeField]
+	private float m_RunSpeed = 4;
+
+	private bool m_IsRunning = false;
+
+	private Vector2 m_MovementDirection = new Vector2(0, 0);
+
+	[Header("Combat")]
+	[SerializeField]
+	private float m_MaxHealth = 100;
+	[SerializeField]
+	private GameObject m_BloodPartical;
+
+	private float m_Health;
+	private Vector2 m_LookDirection = new Vector2(0, 0);
+
+	[HideInInspector]
+	public UnityEvent m_DeathEvent = new UnityEvent();
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		m_Health = m_MaxHealth;
+
+		m_WeaponObject = new GameObject();
+		m_WeaponObject.transform.parent = transform;
+		m_WeaponSprite = m_WeaponObject.AddComponent<SpriteRenderer>();
+
+		// renderer
+		m_Renderer = GetComponent<SpriteRenderer>();
+
+		// movement / physics
+		m_Collider = GetComponent<CircleCollider2D>();
+		m_Rigidbody = GetComponent<Rigidbody2D>();
+
+		m_Collider.radius = 0.5f;
+
+		m_Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+		m_Rigidbody.interpolation = RigidbodyInterpolation2D.None;
+		m_Rigidbody.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		float speed = m_IsRunning ? m_RunSpeed : m_WalkSpeed;
+
+		m_WeaponObject.transform.position = m_LookDirection * 0.5f;
+
+		if (m_MovementDirection.sqrMagnitude >= 0.001f)
+		{
+			m_Renderer.flipX = Vector2.Dot(m_MovementDirection, Vector2.left) > 0;
+
+			m_Rigidbody.velocity = m_MovementDirection * speed;
+		}
+		else
+			m_Rigidbody.velocity = Vector2.zero;
+
+	}
+
+	public void SetSprite(bool sprint)
+	{
+		m_IsRunning = sprint;
+	}
+
+	public void Move(Vector2 direction)
+	{
+		m_MovementDirection = direction.normalized;
+	}
+
+	public void SetLookDirection(Vector2 direction)
+	{
+		m_LookDirection = direction.normalized;
+	}
+
+	public void Damage(float damage)
+	{
+		m_Health -= damage;
+		m_Health = Mathf.Clamp(m_Health, 0, m_MaxHealth);
+
+		GameObject bloodPartical = Instantiate(m_BloodPartical);
+		Destroy(bloodPartical, 5);
+
+		if(m_Health == 0)
+			m_DeathEvent.Invoke();
+	}
+
+	public void Attack()
+	{
+		// TODO
+	}
+
+}
---
+++ Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class PlayerController : MonoBehaviour
+{
+	private Character m_Character;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		m_Character = GetComponent<Character>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 movDir = new Vector2 (
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical")
+		);
+
+		bool sprint = Input.GetKey(KeyCode.LeftShift) | Input.GetKey(KeyCode.RightShift);
+
+		m_Character.Move(movDir);
+		m_Character.SetSprite(sprint);
+		m_Character.SetLookDirection();
+	}
+}
